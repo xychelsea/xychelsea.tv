d59011dd07f3e176ef663a41b06fa30836acb529
Chelsea E. Manning <xychelsea@sshbox.localdomain>
2022-10-15 18:12:18 -0400

    add npm script to build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,17 @@
 
 docker run \
 	--interactive \
+	--name npm-install-postcss-cli \
+	--rm \
+	--tty \
+	--user node \
+	--volume `pwd`:/home/node/app \
+	--workdir /home/node/app \
+	node:18.10.0-alpine3.16 \
+	npm install postcss-cli
+
+docker run \
+	--interactive \
 	--name xychelsea.tv-hugo \
 	--publish 1313:1313 \
 	--rm \
